53ec0d057d3ba26dd8b1ef9b6a8ce1fd27f86422
Hatemii <hatem.xhezairi96@gmail.com>
2020-05-18 19:26:34 +0200

    working with currencies

--- app/src/main/java/com/example/my_first_application/MainActivity.java
+++ app/src/main/java/com/example/my_first_application/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.my_first_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +25,15 @@ public class MainActivity extends AppCompatActivity {
         sp2 = findViewById(R.id.spto);
         b1 = findViewById(R.id.btn1);
 
-        
+
+        String[] from = {"USD","EUR","CHF","LEK"};
+        ArrayAdapter ad = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,from);
+        sp1.setAdapter(ad);
+
+
+        String[] to = {"EUR","LEK","CHF","USD"};
+        ArrayAdapter aa = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,to);
+        sp2.setAdapter(aa);
 
 
 
