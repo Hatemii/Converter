0e413a7d5dd6a62078c3dc62edc4cb6dc2ef146e
Hatemii <hatem.xhezairi96@gmail.com>
2020-05-18 19:18:05 +0200

    declaring variables in main

--- app/src/main/java/com/example/my_first_application/MainActivity.java
+++ app/src/main/java/com/example/my_first_application/MainActivity.java
@@ -6,9 +6,25 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner sp1,sp2;
+    EditText ed1;
+    Button b1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ed1 = findViewById(R.id.txtamount);
+        sp1 = findViewById(R.id.spfrom);
+        sp2 = findViewById(R.id.spto);
+        b1 = findViewById(R.id.btn1);
+
+        
+
+
+
+
     }
 }
