edc5146d0bedcee7f9fef36372ff0bce64dbd588
Hatemii <hatem.xhezairi96@gmail.com>
2020-05-18 19:40:44 +0200

    Functions are ready

--- app/src/main/java/com/example/my_first_application/MainActivity.java
+++ app/src/main/java/com/example/my_first_application/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.my_first_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,12 +33,33 @@ public class MainActivity extends AppCompatActivity {
         sp1.setAdapter(ad);
 
 
-        String[] to = {"EUR","LEK","CHF","USD"};
+        String[] to = {"EUR","LEK","CHF"};
         ArrayAdapter aa = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,to);
         sp2.setAdapter(aa);
 
 
 
-
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double total;
+                Double amount = Double.parseDouble(ed1.getText().toString());
+                        if(sp1.getSelectedItem().toString() == "USD" &&
+                        sp2.getSelectedItem().toString() == "LEK"){
+                            total = amount * 123.3;
+                            Toast.makeText(getApplicationContext(),total.toString(),Toast.LENGTH_LONG).show();
+                        }
+                        else if(sp1.getSelectedItem().toString() == "USD" &&
+                               sp2.getSelectedItem().toString() == "EUR"){
+                                    total = amount * 5.5;
+                                    Toast.makeText(getApplicationContext(),total.toString(),Toast.LENGTH_LONG).show();
+                        }
+                        else if(sp1.getSelectedItem().toString() == "USD" &&
+                                sp2.getSelectedItem().toString() == "CHF"){
+                            total = amount * 10.5;
+                            Toast.makeText(getApplicationContext(),total.toString(),Toast.LENGTH_LONG).show();
+                        }
+            }
+        });
     }
 }
