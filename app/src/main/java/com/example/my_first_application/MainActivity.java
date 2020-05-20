364a7180a6e88a8417a1b9122bce23104e61db65
Hatemii <hatem.xhezairi96@gmail.com>
2020-05-20 16:59:41 +0200

    minor changes

--- app/src/main/java/com/example/my_first_application/MainActivity.java
+++ app/src/main/java/com/example/my_first_application/MainActivity.java
@@ -8,14 +8,15 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner sp1,sp2;
     EditText ed1;
     Button b1;
-
+    TextView f1;
+    private TextView tv_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +29,15 @@ public class MainActivity extends AppCompatActivity {
         b1 = findViewById(R.id.btn1);
 
 
-        String[] from = {"USD","EUR","CHF","LEK"};
+        tv_result = (TextView) findViewById(R.id.tv_result);
+
+
+        String[] from = {"liter","ounces","cups","gallon"};
         ArrayAdapter ad = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,from);
         sp1.setAdapter(ad);
 
 
-        String[] to = {"EUR","LEK","CHF"};
+        String[] to = {"ounces","gallon","cups","liter"};
         ArrayAdapter aa = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,to);
         sp2.setAdapter(aa);
 
@@ -42,24 +46,83 @@ public class MainActivity extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Double total;
+                Double total = 0.0;
                 Double amount = Double.parseDouble(ed1.getText().toString());
-                        if(sp1.getSelectedItem().toString() == "USD" &&
-                        sp2.getSelectedItem().toString() == "LEK"){
-                            total = amount * 123.3;
-                            Toast.makeText(getApplicationContext(),total.toString(),Toast.LENGTH_LONG).show();
+
+
+                        // Liter
+                        if (sp1.getSelectedItem().toString() == "liter" &&
+                                sp2.getSelectedItem().toString() == "gallon") {
+                            total = amount * 0.2641720524;
+
+                        } else if (sp1.getSelectedItem().toString() == "liter" &&
+                                sp2.getSelectedItem().toString() == "ounces") {
+                            total = amount * 33.814022702;
+
+
+                        } else if (sp1.getSelectedItem().toString() == "liter" &&
+                                sp2.getSelectedItem().toString() == "cups") {
+                            total = amount * 4.2267528377;
+
                         }
-                        else if(sp1.getSelectedItem().toString() == "USD" &&
-                               sp2.getSelectedItem().toString() == "EUR"){
-                                    total = amount * 5.5;
-                                    Toast.makeText(getApplicationContext(),total.toString(),Toast.LENGTH_LONG).show();
+
+
+                        // Gallon
+                        else if (sp1.getSelectedItem().toString() == "gallon" &&
+                                sp2.getSelectedItem().toString() == "liter") {
+                            total = amount * 3.785411784;
+
+                        } else if (sp1.getSelectedItem().toString() == "gallon" &&
+                                sp2.getSelectedItem().toString() == "ounces") {
+                            total = amount * 128;
+
+                        } else if (sp1.getSelectedItem().toString() == "gallon" &&
+                                sp2.getSelectedItem().toString() == "cups") {
+                            total = amount * 16;
+
                         }
-                        else if(sp1.getSelectedItem().toString() == "USD" &&
-                                sp2.getSelectedItem().toString() == "CHF"){
-                            total = amount * 10.5;
-                            Toast.makeText(getApplicationContext(),total.toString(),Toast.LENGTH_LONG).show();
+
+
+                        // Ounces
+                        else if (sp1.getSelectedItem().toString() == "ounces" &&
+                                sp2.getSelectedItem().toString() == "liter") {
+                            total = amount * 0.0295735296;
+
+                        } else if (sp1.getSelectedItem().toString() == "ounces" &&
+                                sp2.getSelectedItem().toString() == "gallon") {
+                            total = amount * 0.0078125;
+
+                        } else if (sp1.getSelectedItem().toString() == "ounces" &&
+                                sp2.getSelectedItem().toString() == "cups") {
+                            total = amount * 0.125;
+
                         }
+
+
+                        // Cups
+                        else if (sp1.getSelectedItem().toString() == "cups" &&
+                                sp2.getSelectedItem().toString() == "liter") {
+                            total = amount * 0.2365882365;
+
+                        } else if (sp1.getSelectedItem().toString() == "cups" &&
+                                sp2.getSelectedItem().toString() == "gallon") {
+                            total = amount * 0.0625;
+
+                        } else if (sp1.getSelectedItem().toString() == "cups" &&
+                                sp2.getSelectedItem().toString() == "ounces") {
+                            total = amount * 8;
+
+                        }
+
+
+
+                tv_result.setText("Rezultati: " + total);
+
             }
         });
+
+
+
     }
+
 }
